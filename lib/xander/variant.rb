5d84bce11804dc04f73ca28d39cbc88b0a536503
Martyn Garcia <martyn.garcia@gmail.com>
2013-03-24 16:37:11 -0600

    Variant#site, Variant#impression, Variant#conversion added.

--- lib/xander/variant.rb
+++ lib/xander/variant.rb
@@ -1,4 +1,23 @@
 module Xander
   class Variant < ActiveResource::Base
+    self.site="http://api.xander.io/"
+
+    def impression
+      @attributes[:impressions]||=0
+      @attributes[:impressions]+=1
+    end
+
+    def impressions
+      @attributes[:impressions] || 0
+    end
+
+    def conversion
+      @attributes[:conversions]||=0
+      @attributes[:conversions]+=1
+    end
+
+    def conversions
+      @attributes[:conversions] || 0
+    end
   end
 end
